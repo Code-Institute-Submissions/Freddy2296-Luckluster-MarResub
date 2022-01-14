b2846165ab15399867484ee1cd65b50443f32a3a
Freddy2296 <Garciafreddy328@yahoo.com>
2022-01-14 22:44:39 +0000

    Updated checkout form

--- env.py
+++ env.py
@@ -3,4 +3,4 @@ import os
 os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51K3VWtFcBISWimPaMXRCUDZuYPbfFsXEbEdcEHUzwEm5GU1SY5Eltoj8JwLjvHKaxRnNNzpyBYs7Cir4rbfgUf1Y00tHWseQ49"
 os.environ["STRIPE_SECRET_KEY"] = "sk_test_51K3VWtFcBISWimPa8ABd1ubEfXE2fdLapTp1qQBLAHDn1f6RlMRU0gvSbLrSjq6h8NMz2ApnceDSKzvIY2SCFB5R00gJpZQCkg"
 os.environ["STRIPE_WH_SECRET"] = "whsec_ujBb9T87YWckMBX8dEziiTJ77zdtieVl"
-os.environ["SECRET_KEY"] = "p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7"
+os.environ.setdefault("SECRET_KEY", "p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7")
