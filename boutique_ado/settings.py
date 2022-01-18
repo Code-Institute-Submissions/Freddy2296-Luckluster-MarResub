5a3be576d95c4090ee5e2cc74c1ed410ffb97ffe
Freddy2296 <Garciafreddy328@yahoo.com>
2022-01-18 01:34:07 +0000

    Updated alluath templates and tested profiles

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = []
 
 
@@ -46,6 +47,8 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'profiles',
+    
 
     # Other
     'crispy_forms',
