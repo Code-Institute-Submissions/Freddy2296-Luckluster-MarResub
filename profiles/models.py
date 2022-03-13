0b99e1b3eba345c258895d6a5dd09a16e272c75f
Freddy2296 <Garciafreddy328@yahoo.com>
2022-03-13 20:45:46 +0000

    Added new logo index file and created new img folder

--- profiles/models.py
+++ profiles/models.py
@@ -5,31 +5,76 @@ from django.dispatch import receiver
 
 from django_countries.fields import CountryField
 
+import uuid
+
 
 class UserProfile(models.Model):
     """
-    A user profile model for maintaining default
+    A user profile for maintaining default
     delivery information and order history
     """
+    # each user can only have one profile. And each profile can only
+    # be attached to one user.
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    default_county = models.CharField(max_length=80, null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country', null=True, blank=True)
+
+    # delivery information fields we want the user to be able to
+    # provide defaults for
+    default_phone_number = models.CharField(max_length=20,
+                                            null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80,
+                                               null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80,
+                                               null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40,
+                                            null=True, blank=True)
+    default_county = models.CharField(max_length=80,
+                                      null=True, blank=True)
+    default_postcode = models.CharField(max_length=20,
+                                        null=True, blank=True)
+    default_country = CountryField(blank_label='Country',
+                                   null=True, blank=True)
 
     def __str__(self):
         return self.user.username
 
 
+# A quick receiver for the post save event from the user model.
+# So that each time a user object is saved,  automatically
+# either create a profile for them if the user has just been created.
+# Or just save the profile to update it if the user already existed.
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
-    Create or update the user profile
+    Create or Update the user profile
     """
     if created:
         UserProfile.objects.create(user=instance)
-    # Existing users: just save the profile
+    # Existing Users: just save the profile
     instance.userprofile.save()
+
+
+class SubscriberList(models.Model):
+    subscriber_id = models.CharField(max_length=32,
+                                     null=False,
+                                     editable=False)
+    email = models.EmailField(max_length=70, null=True,
+                              blank=True, unique=True)
+
+    # private method available only to ProductInventory Class
+    def _generate_subscriber_id(self):
+        """
+        Generate a random, unique subscriber ID number
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the subscriber ID
+        if it hasn't been set already.
+        """
+        if not self.subscriber_id:
+            self.subscriber_id = self._generate_subscriber_id()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.subscriber_id
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import UserProfile
+from django.contrib.auth.models import User
+
+from .models import UserProfile, SubscriberList
 from .forms import UserProfileForm
 
 from checkout.models import Order
@@ -10,16 +12,57 @@ from checkout.models import Order
 @login_required
 def profile(request):
     """ Display the user's profile. """
+    subscribed = False
     profile = get_object_or_404(UserProfile, user=request.user)
+    # Get user instance and user email
+    user = get_object_or_404(User, username=request.user)
+    user_email = str(user.email)
+    if request.POST:
+        # POST Request so providing data
+        subTog = request.POST.get('subTogBtn', '')
+        # check if the user has already subscribed,
+        # and if so set subscriber toggle to ON
+        if SubscriberList.objects.filter(email=user_email).exists():
+            # Subscriber Exists
+            if subTog == "on":
+                # Toggle also on, Already Subscribed
+                subscribed = True
+            else:
+                # Already exists but toggle off
+                # Delete subscriber instance.
+                subscriber_instance = get_object_or_404(SubscriberList,
+                                                        email=user_email)
+                subscriber_instance.delete()
+                subscribed = False
+        else:
+            # Subscriber does not already Exist
+            if request.POST.get('subTogBtn', '') == "on":
+                # Toggle on, so set up Subscriber,
+                # create and save subscriber instance.
+                SubscriberList.objects.create(email=user_email)
+                subscribed = True
+            else:
+                # toggle off, so do nothing
+                subscribed = False
+    else:
+        # GET Request, so just looking to view profile
+        if SubscriberList.objects.filter(email=user_email).exists():
+            # Subscriber exists
+            subscribed = True
+        else:
+            # subscriber does not already Exist
+            subscribed = False
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-        else:
-            messages.error(request, 'Update failed. Please ensure the form is valid.')
     else:
+        messages.error(request, 'Profile update Fail. Ensure\
+                                all field are valid')
+    # Use the profile and the related name on the order model
+    # to get the users orders and return those to the template
         form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
@@ -27,15 +70,17 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
-        'on_profile_page': True
+        'on_profile_page': True,
+        'subscribed': subscribed,
     }
 
     return render(request, template, context)
 
 
 def order_history(request, order_number):
+    # get order
     order = get_object_or_404(Order, order_number=order_number)
-
+    # provide message to user
     messages.info(request, (
         f'This is a past confirmation for order number {order_number}. '
         'A confirmation email was sent on the order date.'
@@ -48,3 +93,25 @@ def order_history(request, order_number):
     }
 
     return render(request, template, context)
+
+
+def subscribe_List(request):
+    # get email supplied
+    email = request.POST.get('email', '')
+    print(email)
+    # check if email already exists in Subscriber List
+    print(SubscriberList.objects.filter(email=email).exists())
+
+    if SubscriberList.objects.filter(email=email).exists():
+        # if the email is already on the subscriber list, providse
+        # the user with a notification
+        messages.info(request, ('Subscriber Already Exists'))
+        print("Subscriber Exists")
+        return redirect(reverse('home'))
+    else:
+        # if the email is not already in the subscriber list,
+        # create and save subscriber instance.
+        SubscriberList.objects.create(email=str(email))
+        messages.success(request, ('Thanks for subscribing'))
+        print("Subscriber to be saved")
+        return redirect(reverse('home'))
