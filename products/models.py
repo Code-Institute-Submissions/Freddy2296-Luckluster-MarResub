a08d9e199fc54fe24a5d37be651a68c2b8a40324
Freddy2296 <Garciafreddy328@yahoo.com>
2022-03-16 22:38:45 +0000

    Added new code to products folder and added new files

--- products/models.py
+++ products/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+from random import randint
+import uuid
 
 
 class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -17,15 +21,104 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2,
+                                 null=True, blank=True)
+    stock_level = models.IntegerField(validators=[MinValueValidator(0)],
+                                      null=False, default=0)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    # private method available only to Product Class
+    def _generate_sku_number(self):
+        """
+        Generate a random, unique 10 digit SKU number starting with CB
+        """
+        range_start = 10**(10-1)
+        range_end = (10**10)-1
+        sku_num = randint(range_start, range_end)
+        sku_str = "CB"+str(sku_num)
+        return sku_str
+
+    def save(self, *args, **kwargs):
+        """
+        Override the product save method to generate the SKU number
+        if it hasn't been set already.
+        """
+        if not self.sku:
+            self.sku = self._generate_sku_number()
+        super().save(*args, **kwargs)
+
+
+class ProductReview(models.Model):
+    review_id = models.CharField(max_length=254, null=True, blank=True)
+    user = models.CharField(max_length=254, null=False)
+    # product = models.CharField(max_length=254, null=False)
+    product = models.ForeignKey('Product', null=True, 
+                                blank=True, on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[MinValueValidator(0),
+                                             MaxValueValidator(5)],
+                                 null=False, default=0)
+    review = models.TextField()
+    timestamp = models.CharField(max_length=254, null=True)
+
+    # private method available only to ProductReview Class
+    def _generate_review_number(self):
+        """
+        Generate a random, unique 10 digit Product Review
+        (PR) number starting with PR
+        """
+        range_start = 10**(10-1)
+        range_end = (10**10)-1
+        pr_num = randint(range_start, range_end)
+        pr_str = "PR"+str(pr_num)
+        return pr_str
+
+    def save(self, *args, **kwargs):
+        """
+        Override the save method to generate the PR number number
+        """
+        if not self.review_id:
+            self.review_id = self._generate_review_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.review_id
+
+
+class ProductInventory(models.Model):
+    inventory_number = models.CharField(max_length=32,
+                                        null=False,
+                                        editable=False)
+    product = models.ForeignKey('Product', null=True, blank=True,
+                                on_delete=models.CASCADE)
+    stock_level = models.IntegerField(validators=[MinValueValidator(0)],
+                                      null=False, default=0)
+
+    # private method available only to ProductInventory Class
+    def _generate_Inventory_number(self):
+        """
+        Generate a random, unique inventory number
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.inventory_number:
+            self.inventory_number = self._generate_Inventory_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.inventory_id
--- products/views.py
+++ products/views.py
@@ -3,14 +3,18 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
-
-from .models import Product, Category
+from .models import Product, Category, ProductReview
 from .forms import ProductForm
+
+from checkout.models import Order
+
+from datetime import datetime
 
 # Create your views here.
 
+
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ View to return all products """
 
     products = Product.objects.all()
     query = None
@@ -21,6 +25,7 @@ def all_products(request):
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
+            # sort used here to preserve original field name
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
@@ -32,8 +37,10 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-            
+
         if 'category' in request.GET:
+            # if category is in the GET, split the categories based on
+            # the ',' in the url. the filter based on specified category
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
@@ -41,9 +48,13 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(request, "You didn't enter any\
+                                search parameters")
                 return redirect(reverse('products'))
-            
+
+            # Q object allows us to bring an OR operation into the search.
+            # This verifies if the search criteria appears in either the name
+            # or the descriptions field.
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -60,12 +71,14 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to show individual product details """
+    """ Return product and it's reviews and pass to product_detail.html """
 
     product = get_object_or_404(Product, pk=product_id)
+    productReviews = ProductReview.objects.filter(product=product_id)
 
     context = {
         'product': product,
+        'reviews': productReviews,
     }
 
     return render(request, 'products/product_detail.html', context)
@@ -74,21 +87,28 @@ def product_detail(request, product_id):
 @login_required
 def add_product(request):
     """ Add a product to the store """
+    # If the user is not a superuser redirect them back to the home
+    # page with the message that only store owners can perform this function.
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
 
+    # if method is POST, create new instance of form and include
+    # request files (images)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
+        # if valid, save and present a success message
         if form.is_valid():
             product = form.save()
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            # if there was an error, present notification to user
+            messages.error(request, 'Failed to add product. Please\
+                                     ensure the form is valid.')
     else:
         form = ProductForm()
-        
+
     template = 'products/add_product.html'
     context = {
         'form': form,
@@ -99,20 +119,25 @@ def add_product(request):
 
 @login_required
 def edit_product(request, product_id):
-    """ Edit a product in the store """
+    """ Edit a product """
+    # If the user is not a superuser redirect them back to the home
+    # page with the message that only store owners can perform this function.
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
 
+    # get product based on the ID thats been passed in,
+    # then populate the form with the details of the product
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Successfully updated product!')
+            messages.success(request, 'Product Updated!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+            messages.error(request, 'Product Update Failed. Please\
+                                    ensure the form is valid.')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
@@ -128,7 +153,9 @@ def edit_product(request, product_id):
 
 @login_required
 def delete_product(request, product_id):
-    """ Delete a product from the store """
+    """ Delete a product """
+    # If the user is not a superuser redirect them back to the home
+    # page with the message that only store owners can perform this function.
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
@@ -137,3 +164,68 @@ def delete_product(request, product_id):
     product.delete()
     messages.success(request, 'Product deleted!')
     return redirect(reverse('products'))
+
+
+def edit_product_inventory(product_id, quantity):
+    """ Edit a product inventory count"""
+    # get product based on the ID thats been passed in,
+    # the calculate the new stock count by subtracting the
+    # order quantity from the stock_level
+    product = get_object_or_404(Product, pk=product_id)
+    updated_stock = product.stock_level - quantity
+    product.stock_level = updated_stock
+    product.save()
+    print('Product Updated!')
+    return
+
+
+def product_review(request, order_number):
+    # get order
+    order = get_object_or_404(Order, order_number=order_number)
+    template = 'products/product_review.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+    return render(request, template, context)
+
+
+def add_review(request):
+    if request.method == 'POST':
+        # Gather Review Elements from Review (PID, Rating & Review text)
+        user = request.user
+        pid = request.POST.get('pid', '')
+        rating = request.POST.get('rating', '')
+        review = request.POST.get('review', '')
+        # get product instance
+        product = get_object_or_404(Product, pk=pid)
+
+        # get Timestamp for review submission in local date and time
+        dateTimeObj = datetime.now()
+        timestampStr = dateTimeObj.strftime("%d-%b-%Y (%H:%M)")
+
+        # Create and Save Product Review instance
+        ProductReview.objects.create(user=str(user),
+                                     product=product,
+                                     rating=rating,
+                                     review=review,
+                                     timestamp=timestampStr)
+
+        # We now need to update the product rating based on the review input
+        # First get all reviews related to that product
+        allReviews = ProductReview.objects.filter(product=pid)
+        # Number of Reviews for that product
+        reviewtotal = allReviews.count()
+        # Get sum of all ratings
+        sumRating = 0
+        for reviews in allReviews:
+            sumRating += reviews.rating
+
+        # Average rating is total Sum / number of reviews
+        averageRating = sumRating/reviewtotal
+        product.rating = averageRating
+        product.save()
+
+        messages.success(request, 'Review Submission Successful!')
+        # Return the user to their profile page
+        return redirect(reverse('profile'))
